c168ec30d5909bb1a323128b6c033485cda05352
cvencek_jan <jan.cvencek@goodrequest.com>
2022-03-14 11:41:17 +0100

    Move refreshing of tokens into root level component

--- src/routes/AuthRoute.tsx
+++ src/routes/AuthRoute.tsx
@@ -1,20 +1,16 @@
-import React, { FC, useEffect } from 'react'
+import React, { FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Redirect, RouteProps } from 'react-router-dom'
 import IdleTimer from 'react-idle-timer'
 import { Dictionary } from 'lodash'
-import { useDispatch } from 'react-redux'
 
 // routes
 import BaseRoute from './BaseRoute'
 
 // utils
 import { isLoggedIn } from '../utils/auth'
-import { PAGE, SUBMENU_PARENT, REFRESH_TOKEN_INTERVAL, REFRESH_PAGE_INTERVAL } from '../utils/enums'
+import { PAGE, SUBMENU_PARENT, REFRESH_PAGE_INTERVAL } from '../utils/enums'
 import { getPath } from '../utils/history'
-
-// redux
-import { refreshToken } from '../reducers/users/userActions'
 
 type Props = RouteProps & {
 	layout: React.ReactNode
@@ -27,7 +23,6 @@ type Props = RouteProps & {
 	}
 	/** e.g. tabKey or other extra props for page */
 	extra?: Dictionary<any>
-	redirectTo?: string
 }
 
 const onIdle = () => {
@@ -36,25 +31,9 @@ const onIdle = () => {
 
 const AuthRoute: FC<Props> = (props) => {
 	const [t] = useTranslation()
-	const dispatch = useDispatch()
-	const { redirectTo } = props
-
-	useEffect(() => {
-		const refreshInterval = setInterval(dispatch(refreshToken), REFRESH_TOKEN_INTERVAL)
-
-		return () => {
-			if (refreshInterval) {
-				clearInterval(refreshInterval)
-			}
-		}
-	}, [dispatch])
 
 	if (!isLoggedIn()) {
 		return <Redirect to={getPath(t('paths:login'))} />
-	}
-
-	if (redirectTo) {
-		return <Redirect to={redirectTo} />
 	}
 
 	return (

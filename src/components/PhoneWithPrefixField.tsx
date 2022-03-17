f39fd59088f07c14d8368f8b581fa4fa819a3393
Matej Capkovic <matej.capkovic@goodrequest.com>
2022-03-17 08:15:20 +0100

    NOT-249 - add phone fallback

--- src/components/PhoneWithPrefixField.tsx
+++ src/components/PhoneWithPrefixField.tsx
@@ -18,14 +18,28 @@ type Props = {
 	phoneName?: string
 }
 
+const fallbackOptions = [{ key: 1, label: '+421', value: 'SK', flag: 'https://flagcdn.com/h20/sk.png' }]
+const fallbackDefaultValue = 'SK'
+
 const PhoneWithPrefixField = (props: Props) => {
 	const { placeholder, label, size, prefixName = 'phonePrefixCountryCode', phoneName = 'phone' } = props
 	const prefixOptions = useSelector((state: RootState) => state.enumerationsStore.countries)
 
+	let options = prefixOptions.enumerationsOptions
+	if (options.length === 0) options = fallbackOptions
+
 	return (
 		<Row gutter={8} wrap={false}>
 			<Col flex='102px'>
-				<Field label={label} component={PhonePrefixField} name={prefixName} size={size} options={prefixOptions.enumerationsOptions} loading={prefixOptions.isLoading} />
+				<Field
+					label={label}
+					component={PhonePrefixField}
+					name={prefixName}
+					size={size}
+					options={options}
+					loading={prefixOptions.isLoading}
+					defaultValue={fallbackDefaultValue}
+				/>
 			</Col>
 			<Col flex='auto'>
 				<Field label={label ? ' ' : undefined} component={InputField} placeholder={placeholder} name={phoneName} size={size} />

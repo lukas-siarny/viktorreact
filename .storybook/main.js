9bea85760f2351a77b1a222e581d388cd03ab1cb
Matej Capkovic <matej.capkovic@goodrequest.com>
2022-04-07 17:45:31 +0200

    NOT-155 - format file

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,12 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app",
-	"storybook-addon-designs",
-	// "storybook-addon-customize-antd-theme"
-  ],
-  "framework": "@storybook/react"
+	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+		'@storybook/preset-create-react-app',
+		'storybook-addon-designs'
+		// "storybook-addon-customize-antd-theme"
+	],
+	framework: '@storybook/react'
 }

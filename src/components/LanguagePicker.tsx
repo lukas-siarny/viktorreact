e8f6facadc4378aac86e9fd2b1faad5f6ac2c48f
cvencek_jan <jan.cvencek@goodrequest.com>
2022-05-04 16:39:59 +0200

    Edit: use languagePicker as Select component not Form

--- src/components/LanguagePicker.tsx
+++ src/components/LanguagePicker.tsx
@@ -1,50 +1,36 @@
-import React, { FC, useEffect } from 'react'
-import { reduxForm, InjectedFormProps, Field, initialize } from 'redux-form'
-import { Form } from 'antd'
+import React, { FC } from 'react'
+import { Select } from 'antd'
 import i18next from 'i18next'
-import { useDispatch } from 'react-redux'
-
-// // interfaces
-import { ILanguagePickerForm } from '../types/interfaces'
 
 // utils
-import { FORM, LANGUAGE } from '../utils/enums'
+import { LANGUAGE } from '../utils/enums'
 
-// atoms
-import SelectField from '../atoms/SelectField'
+const { Option } = Select
 
 const options = Object.values(LANGUAGE).map((value) => ({ label: value, value }))
 
-type ComponentProps = {
+type Props = {
 	className?: string
 }
 
-type Props = InjectedFormProps<ILanguagePickerForm, ComponentProps> & ComponentProps
-
 const LanguagePicker: FC<Props> = (props) => {
 	const { className } = props
-	const dispatch = useDispatch()
-
-	useEffect(() => {
-		dispatch(initialize(FORM.LANGUAGE_FORM, { language: i18next.language }))
-	}, [dispatch])
 
 	const handleLanguageChange = (value: any) => {
 		i18next.changeLanguage(value)
 	}
 
 	return (
-		<Form className={className} layout='vertical'>
-			<Field className='m-0' component={SelectField} name='language' options={options} onChange={handleLanguageChange} />
-		</Form>
+		<div className={className}>
+			<Select defaultValue={i18next.language} onChange={handleLanguageChange} className={'noti-select-input'} dropdownClassName={'noti-select-dropdown'}>
+				{options.map((option: any, index: number) => (
+					<Option value={option.value} key={index}>
+						{option.label}
+					</Option>
+				))}
+			</Select>
+		</div>
 	)
 }
 
-const form = reduxForm<any, ComponentProps>({
-	form: FORM.LANGUAGE_FORM,
-	forceUnregisterOnUnmount: false,
-	touchOnChange: true,
-	destroyOnUnmount: false
-})(LanguagePicker)
-
-export default form
+export default LanguagePicker

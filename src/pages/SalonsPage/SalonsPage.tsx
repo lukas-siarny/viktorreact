3c6abb379345f0655c81742352fe49f0ecf5c0ee
cvencek_jan <jan.cvencek@goodrequest.com>
2022-05-04 11:09:32 +0200

    Removed unused import

--- src/pages/SalonsPage/SalonsPage.tsx
+++ src/pages/SalonsPage/SalonsPage.tsx
@@ -1,7 +1,7 @@
 import { useTranslation } from 'react-i18next'
 import { useDispatch, useSelector } from 'react-redux'
 import { compose } from 'redux'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { ArrayParam, NumberParam, StringParam, useQueryParams, withDefault } from 'use-query-params'
 import { Col, Progress, Row } from 'antd'
 import { ColumnsType } from 'antd/lib/table'

868743b4e87a4b31a3c9ef188177a738e4c42598
cvencek_jan <jan.cvencek@goodrequest.com>
2022-05-12 14:04:35 +0200

    Update auth tests

--- cypress/integration/1-users/auth.spec.ts
+++ cypress/integration/1-users/auth.spec.ts
@@ -4,35 +4,36 @@ import Cypress from 'cypress'
 import { FORM } from '../../../src/utils/enums'
 import { generateRandomString } from '../../support/helpers'
 
-const credentials = require('../../fixtures/credentials.json')
-const user = require('../../fixtures/user.json')
+import credentials from '../../fixtures/credentials.json'
+import user from '../../fixtures/user.json'
 
 context('Auth', () => {
+	// TODO
 	it('Sign up', () => {
 		cy.clearLocalStorage()
 		cy.intercept({
 			method: 'POST',
-			url: '/api/b2b/admin/auth/login',
+			url: '/api/b2b/admin/users/registration'
 		}).as('registration')
 		cy.visit('/signup')
-		cy.setInputValue(FORM.REGISTRATION, 'email',  `${generateRandomString(5)}${user.email}`)
-		cy.setInputValue(FORM.REGISTRATION, 'password',  user.password)
-		cy.setInputValue(FORM.REGISTRATION, 'confirmPassword',  user.password)
-		cy.setInputValue(FORM.REGISTRATION, 'phone',  user.phone)
-		cy.clickButton(FORM.REGISTRATION, 'gdpr', true)
-		cy.clickButton(FORM.REGISTRATION, 'gtc', true)
+		cy.setInputValue(FORM.REGISTRATION, 'email', `${generateRandomString(5)}${user.email}`)
+		cy.setInputValue(FORM.REGISTRATION, 'password', user.password)
+		cy.setInputValue(FORM.REGISTRATION, 'confirmPassword', user.password)
+		cy.setInputValue(FORM.REGISTRATION, 'phone', user.phone)
+		cy.clickButton('gdpr', FORM.REGISTRATION, true)
+		cy.clickButton('gtc', FORM.REGISTRATION, true)
 	})
 
 	it('Sign in', () => {
 		cy.clearLocalStorage()
 		cy.intercept({
 			method: 'POST',
-			url: '/api/b2b/admin/auth/login',
+			url: '/api/b2b/admin/auth/login'
 		}).as('authLogin')
 		cy.visit('/login')
 		// TODO - load credentials from process env Cypress.env('auth_email') Cypress.env('auth_password')
-		cy.setInputValue(FORM.LOGIN, 'email',  credentials.email)
-		cy.setInputValue(FORM.LOGIN, 'password',  credentials.password)
+		cy.setInputValue(FORM.LOGIN, 'email', credentials.email)
+		cy.setInputValue(FORM.LOGIN, 'password', credentials.password)
 		cy.get('form').submit()
 		cy.wait('@authLogin').then((interception: any) => {
 			// check status code of login request

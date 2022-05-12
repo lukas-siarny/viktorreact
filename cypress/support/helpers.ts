47d32c9dee9f5c35ef233324138cb6b7d0bba6dd
cvencek_jan <jan.cvencek@goodrequest.com>
2022-05-12 14:06:00 +0200

    Add helper fn for random integer with custom count of digits

--- cypress/support/helpers.ts
+++ cypress/support/helpers.ts
@@ -1,4 +1,9 @@
+export const generateElementId = (key: string, form?: string) => (form ? `#${form}-${key}` : `#${key}`)
 
-export const generateElementId = (key: string, form?: string) => form ? `#${form}-${key}` : `#${key}`
+export const generateRandomString = (length: number) =>
+	Math.random()
+		.toString(36)
+		.replace(/[^a-z]+/g, '')
+		.substr(0, length)
 
-export const generateRandomString = (length: number) => Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, length)
+export const generateRandomInt = (digits: number) => Math.floor(10 ** (digits - 1) + Math.random() * 9 * 10 ** (digits - 1))

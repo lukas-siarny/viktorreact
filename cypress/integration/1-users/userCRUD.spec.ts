1b924c4048eef6c56fde7251570dc79cc9a1daaa
cvencek_jan <jan.cvencek@goodrequest.com>
2022-05-12 14:07:28 +0200

    Update tests for users

--- cypress/integration/1-users/userCRUD.spec.ts
+++ cypress/integration/1-users/userCRUD.spec.ts
@@ -1,11 +1,11 @@
 import { generateRandomString } from '../../support/helpers'
 import { FORM } from '../../../src/utils/enums'
 
-const user = require('../../fixtures/user.json')
+import user from '../../fixtures/user.json'
 
 context('User', () => {
 	// id of created user
-	let userID: number = 0
+	let userID = 0
 	beforeEach(() => {
 		// restore local storage with tokens from snapshot
 		cy.restoreLocalStorage()
@@ -18,9 +18,9 @@ context('User', () => {
 
 	it('Update my account info', () => {
 		cy.visit('/my-account')
-		cy.setInputValue(FORM.USER_ACCOUNT, 'firstName',  user.firstName, true)
-		cy.setInputValue(FORM.USER_ACCOUNT, 'lastName',  user.lastName, true)
-		cy.setInputValue(FORM.USER_ACCOUNT, 'phone',  user.phone, true)
+		cy.setInputValue(FORM.USER_ACCOUNT, 'firstName', user.firstName, true)
+		cy.setInputValue(FORM.USER_ACCOUNT, 'lastName', user.lastName, true)
+		cy.setInputValue(FORM.USER_ACCOUNT, 'phone', user.phone, true)
 		cy.get('form').submit()
 		cy.checkSuccessToastMessage()
 	})
@@ -28,11 +28,11 @@ context('User', () => {
 	it('Create partner as ADMIN', () => {
 		cy.intercept({
 			method: 'POST',
-			url: '/api/b2b/admin/users',
+			url: '/api/b2b/admin/users'
 		}).as('createPartner')
 		cy.visit('/users/create')
-		cy.setInputValue(FORM.ADMIN_CREATE_USER, 'email',  `${generateRandomString(5)}${user.email}`)
-		cy.setInputValue(FORM.ADMIN_CREATE_USER, 'phone',  user.phone)
+		cy.setInputValue(FORM.ADMIN_CREATE_USER, 'email', `${generateRandomString(5)}${user.email}`)
+		cy.setInputValue(FORM.ADMIN_CREATE_USER, 'phone', user.phone)
 		cy.selectOptionDropdown(FORM.ADMIN_CREATE_USER, 'roleID', 'Partner')
 		cy.get('form').submit()
 		cy.wait('@createPartner').then((interception: any) => {
@@ -44,33 +44,34 @@ context('User', () => {
 		})
 	})
 
-	it('Update partner info as ADMIN', () => {
-		cy.intercept({
-			method: 'PATCH',
-			url: `/api/b2b/admin/users/${userID}`,
-		}).as('updateUser')
-		cy.visit(`/users/${userID}`)
-		cy.setInputValue(FORM.USER_ACCOUNT, 'firstName',  user.firstName)
-		cy.setInputValue(FORM.USER_ACCOUNT, 'lastName',  user.lastName)
-		cy.setInputValue(FORM.USER_ACCOUNT, 'companyName',  user.companyName)
-		cy.setInputValue(FORM.USER_ACCOUNT, 'businessID',  user.businessID)
-		cy.setInputValue(FORM.USER_ACCOUNT, 'street',  user.street)
-		cy.setInputValue(FORM.USER_ACCOUNT, 'city',  user.city)
-		cy.setInputValue(FORM.USER_ACCOUNT, 'zipCode',  user.zipCode)
-		cy.selectOptionDropdown(FORM.USER_ACCOUNT, 'countryCode')
-		cy.get('form').submit()
-		cy.wait('@updateUser').then((interception: any) => {
-			// check status code of login request
-			expect(interception.response.statusCode).to.equal(200)
-			// check conf toast message
-			cy.checkSuccessToastMessage()
-		})
-	})
+	// TODO - company data will be moved to Salon
+	// it('Update partner info as ADMIN', () => {
+	// 	cy.intercept({
+	// 		method: 'PATCH',
+	// 		url: `/api/b2b/admin/users/${userID}`,
+	// 	}).as('updateUser')
+	// 	cy.visit(`/users/${userID}`)
+	// 	cy.setInputValue(FORM.USER_ACCOUNT, 'firstName',  user.firstName)
+	// 	cy.setInputValue(FORM.USER_ACCOUNT, 'lastName',  user.lastName)
+	// 	cy.setInputValue(FORM.USER_ACCOUNT, 'companyName',  user.companyName)
+	// 	cy.setInputValue(FORM.USER_ACCOUNT, 'businessID',  user.businessID)
+	// 	cy.setInputValue(FORM.USER_ACCOUNT, 'street',  user.street)
+	// 	cy.setInputValue(FORM.USER_ACCOUNT, 'city',  user.city)
+	// 	cy.setInputValue(FORM.USER_ACCOUNT, 'zipCode',  user.zipCode)
+	// 	cy.selectOptionDropdown(FORM.USER_ACCOUNT, 'countryCode')
+	// 	cy.get('form').submit()
+	// 	cy.wait('@updateUser').then((interception: any) => {
+	// 		// check status code of login request
+	// 		expect(interception.response.statusCode).to.equal(200)
+	// 		// check conf toast message
+	// 		cy.checkSuccessToastMessage()
+	// 	})
+	// })
 
 	it('Delete partner as ADMIN', () => {
 		cy.intercept({
 			method: 'DELETE',
-			url: `/api/b2b/admin/users/${userID}`,
+			url: `/api/b2b/admin/users/${userID}`
 		}).as('deleteUser')
 		cy.visit(`/users/${userID}`)
 		cy.clickDeleteButtonWithConf(FORM.USER_ACCOUNT)

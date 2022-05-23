2b5dcb963f69be714c52a96494a0db5a97b5cdb3
HaluskaRoman <roman.haluska@goodrequest.com>
2022-05-23 13:36:52 +0200

    NOT-708 add filter

--- src/pages/UsersPage/components/AdminUsersFilter.tsx
+++ src/pages/UsersPage/components/AdminUsersFilter.tsx
@@ -3,14 +3,22 @@ import { Field, InjectedFormProps, reduxForm } from 'redux-form'
 import { Button, Col, Form, Row } from 'antd'
 import { useTranslation } from 'react-i18next'
 import { debounce } from 'lodash'
+import { useSelector } from 'react-redux'
 import { ReactComponent as PlusIcon } from '../../../assets/icons/plus-icon.svg'
 
 // utils
 import { FIELD_MODE, FORM, ROW_GUTTER_X_DEFAULT } from '../../../utils/enums'
-import { validationString } from '../../../utils/helper'
+import { checkFiltersSizeWithoutSearch, validationString } from '../../../utils/helper'
 
 // atoms
 import InputField from '../../../atoms/InputField'
+import SelectField from '../../../atoms/SelectField'
+
+// components
+import Filters from '../../../components/Filters'
+
+// reducers
+import { RootState } from '../../../reducers'
 
 type ComponentProps = {
 	createUser: Function
@@ -29,27 +37,47 @@ const AdminUsersFilter = (props: Props) => {
 	const { handleSubmit, createUser } = props
 	const [t] = useTranslation()
 
+	const form = useSelector((state: RootState) => state.form?.[FORM.ADMIN_USERS_FILTER])
+	const roles = useSelector((state: RootState) => state.roles.roles)
+
+	const searchInput = (
+		<Field
+			className={'h-10 p-0 m-0'}
+			component={InputField}
+			size={'large'}
+			placeholder={t('loc:Hľadať podľa meno, e-mail, tel. číslo')}
+			name='search'
+			fieldMode={FIELD_MODE.FILTER}
+			search
+			validate={fixLength100}
+		/>
+	)
+
+	const customContent = (
+		<Button onClick={() => createUser()} type='primary' htmlType='button' className={'noti-btn w-full'} icon={<PlusIcon />}>
+			{t('loc:Pridať používateľa')}
+		</Button>
+	)
+
 	return (
 		<Form layout='horizontal' onSubmitCapture={handleSubmit} className={'pt-0'}>
-			<Row className={'flex justify-between'} gutter={ROW_GUTTER_X_DEFAULT}>
-				<Col span={6}>
-					<Field
-						className={'h-10 p-0 m-0'}
-						component={InputField}
-						size={'large'}
-						placeholder={t('loc:Hľadať podľa meno, e-mail, tel. číslo')}
-						name='search'
-						fieldMode={FIELD_MODE.FILTER}
-						search
-						validate={fixLength100}
-					/>
-				</Col>
-				<Col span={6}>
-					<Button onClick={() => createUser()} type='primary' htmlType='button' className={'noti-btn w-full'} icon={<PlusIcon />}>
-						{t('loc:Pridať používateľa')}
-					</Button>
-				</Col>
-			</Row>
+			<Filters customContent={customContent} search={searchInput} activeFilters={checkFiltersSizeWithoutSearch(form?.values)}>
+				<Row gutter={ROW_GUTTER_X_DEFAULT}>
+					<Col span={8}>
+						<Field
+							component={SelectField}
+							name={'roleID'}
+							placeholder={t('loc:Rola')}
+							allowClear
+							size={'middle'}
+							filterOptions
+							onDidMountSearch
+							options={roles?.data}
+							loadinf={roles?.isLoading}
+						/>
+					</Col>
+				</Row>
+			</Filters>
 		</Form>
 	)
 }

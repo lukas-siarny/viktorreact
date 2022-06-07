6912b4745fa4368ae9afb7806bdd16edc4b0155e
cvencek_jan <jan.cvencek@goodrequest.com>
2022-06-07 13:19:40 +0200

    Extend avatar group component

--- src/components/AvatarComponents.tsx
+++ src/components/AvatarComponents.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import { Avatar } from 'antd'
+import { Avatar, Popover } from 'antd'
 import { AvatarProps, GroupProps } from 'antd/lib/avatar'
+import { SizeContextProvider } from 'antd/lib/avatar/SizeContext'
+import { PopoverProps } from 'antd/lib/popover'
 
 type UserAvatarProps = AvatarProps & {
 	className?: string
@@ -35,19 +37,54 @@ const UserAvatar = (props: UserAvatarProps) => {
 	)
 }
 
-type AvatarGroupProps = GroupProps & {
+type AvatarGroupProps = Omit<GroupProps, 'maxPopoverPlacement'> & {
 	avatars: UserAvatarProps[]
 	className?: string
+	popoverContent?: React.ReactNode
+	maxPopoverPlacement?: PopoverProps['placement']
 }
 
+/**
+ * Based on AntD implementation - add option to define custom popover content
+ * @link https://github.com/ant-design/ant-design/blob/master/components/avatar/group.tsx
+ */
 export const AvatarGroup = (props: AvatarGroupProps) => {
-	const { avatars, className, maxCount, maxPopoverPlacement, maxPopoverTrigger, maxStyle, size } = props
+	const { avatars, className, maxCount, maxPopoverPlacement, maxPopoverTrigger, maxStyle, size, popoverContent } = props
+
+	const childrenWithProps = avatars.map((avatarProps, index) => <UserAvatar key={index} {...avatarProps} />)
+
+	const numOfChildren = avatars.length
+
+	if (maxCount && maxCount < numOfChildren) {
+		const childrenShow = childrenWithProps.slice(0, maxCount)
+
+		let content = popoverContent
+
+		if (!popoverContent) {
+			content = avatars.map((avatarProps, index) => (
+				<div key={index} className='mb-2'>
+					<UserAvatar {...avatarProps} size={'small'} />
+					<span className='s-regular pl-2'>{avatarProps.text}</span>
+				</div>
+			))
+		}
+
+		childrenShow.push(<Avatar key={'max-count-indicator-key'} style={maxStyle}>{`+${numOfChildren - maxCount}`}</Avatar>)
+
+		return (
+			<SizeContextProvider size={size}>
+				<Popover key='avatar-popover-key' content={content} trigger={maxPopoverTrigger} placement={maxPopoverPlacement}>
+					<div className={`ant-avatar-group ${props.className}`} style={props.style}>
+						{childrenShow}
+					</div>
+				</Popover>
+			</SizeContextProvider>
+		)
+	}
 
 	return (
-		<Avatar.Group className={className} maxCount={maxCount} maxPopoverPlacement={maxPopoverPlacement} maxPopoverTrigger={maxPopoverTrigger} maxStyle={maxStyle} size={size}>
-			{avatars.map((avatarProps) => (
-				<UserAvatar {...avatarProps} />
-			))}
+		<Avatar.Group className={className} size={size}>
+			{childrenWithProps}
 		</Avatar.Group>
 	)
 }

e4281e17a0f0837700daef333d7fe83035a1b87e
cvencek_jan <jan.cvencek@goodrequest.com>
2022-06-10 09:59:28 +0200

    Change avatar size in table view

--- src/pages/ServicesPage/ServicesPage.tsx
+++ src/pages/ServicesPage/ServicesPage.tsx
@@ -106,7 +106,7 @@ const ServicesPage = () => {
 			title: t('loc:Zamestnanec'),
 			dataIndex: 'employees',
 			key: 'employees',
-			render: (value: IUserAvatar[]) => (value ? <AvatarGroup maxCount={3} avatars={value} maxPopoverPlacement={'right'} /> : null)
+			render: (value: IUserAvatar[]) => (value ? <AvatarGroup maxCount={3} avatars={value} maxPopoverPlacement={'right'} size={'small'} /> : null)
 		},
 		{
 			title: t('loc:Trvanie (min)'),

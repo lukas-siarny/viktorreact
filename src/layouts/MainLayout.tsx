a2c26ec28f3705a9a8b646f175e5458516d21f47
cvencek_jan <jan.cvencek@goodrequest.com>
2022-07-20 13:38:42 +0200

    Add fallback for images

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -47,7 +47,7 @@ const MainLayout: FC<Props> = (props) => {
 					className={cx({ 'ant-menu-item-selected': selectedSalon?.id === item.value }, 'py-2-5 px-2 mb-2 font-medium min-w-0')}
 					onClick={() => dispatch(selectSalon(item.value as number))}
 				>
-					<AvatarComponents src={item.logo || SalonDefaultAvatar} size={24} className={'mr-2-5'} />
+					<AvatarComponents src={item.logo} fallBackSrc={SalonDefaultAvatar} size={24} className={'mr-2-5'} />
 					{item.label}
 				</Menu.Item>
 			))}
@@ -62,7 +62,7 @@ const MainLayout: FC<Props> = (props) => {
 		const content = (
 			<Row className={cx('m-2 flex items-center gap-2 min-w-0')} justify='space-between' wrap={false}>
 				<Row wrap={false} className={'min-w-0 flex items-center gap-2-5'}>
-					<AvatarComponents size={24} src={selectedSalon?.logo?.resizedImages.thumbnail || SalonDefaultAvatar} />
+					<AvatarComponents size={24} src={selectedSalon?.logo?.resizedImages.thumbnail} fallBackSrc={SalonDefaultAvatar}/>
 					{selectedSalon?.name && <span className='truncate leading-4 min-w-0 inline-block'>{selectedSalon.name}</span>}
 				</Row>
 

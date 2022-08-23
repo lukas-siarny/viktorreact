d9df625044bcd93928930e965542aa9c2e59e78b
HaluskaRoman <roman.haluska@goodrequest.com>
2022-08-23 20:54:09 +0200

    NOT-1530 refactor compare and history

--- src/pages/SalonsPage/components/SalonHistory.tsx
+++ src/pages/SalonsPage/components/SalonHistory.tsx
@@ -1,13 +1,13 @@
 import React, { FC, ReactNode, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Timeline, Row, Col, Pagination, Spin } from 'antd'
-import { isEmpty } from 'lodash'
+import { Row, Pagination, Spin, List, Divider } from 'antd'
 import cx from 'classnames'
 import { NumberParam, useQueryParams, withDefault, StringParam } from 'use-query-params'
 
 // components
 import { initialize } from 'redux-form'
 import SalonHistoryFilter, { ISalonHistoryFilter } from './SalonHistoryFilter'
+import CompareComponent from '../../../components/CompareComponent'
 
 // types
 import { SalonSubPageProps } from '../../../types/interfaces'
@@ -26,6 +26,21 @@ import { ReactComponent as ResetIcon } from '../../../assets/icons/reset-icon.sv
 import { ReactComponent as EditIcon } from '../../../assets/icons/edit-icon.svg'
 import { ReactComponent as CloseIcon } from '../../../assets/icons/close-icon.svg'
 
+const setIcon = (operation: SALON_HISTORY_OPERATIONS): undefined | ReactNode => {
+	switch (operation) {
+		case SALON_HISTORY_OPERATIONS.DELETE:
+			return <CloseIcon className={'history-icon danger'} />
+		case SALON_HISTORY_OPERATIONS.INSERT:
+			return <CheckIcon width={24} height={24} className={'history-icon success'} />
+		case SALON_HISTORY_OPERATIONS.RESTORE:
+			return <ResetIcon className={'history-icon info'} />
+		case SALON_HISTORY_OPERATIONS.UPDATE:
+			return <EditIcon className={'history-icon warning'} />
+		default:
+			return undefined
+	}
+}
+
 const SalonHistory: FC<SalonSubPageProps> = (props) => {
 	const dispatch = useDispatch()
 	const { salonID } = props
@@ -41,7 +56,8 @@ const SalonHistory: FC<SalonSubPageProps> = (props) => {
 	const salonHistory = useSelector((state: RootState) => state.salons.salonHistory)
 
 	const fetchData = async () => {
-		dispatch(getSalonHistory({ dateFrom: query.dateFrom, dateTo: query.dateTo, salonID, page: query.page, limit: query.limit }))
+		const { data } = await dispatch(getSalonHistory({ dateFrom: query.dateFrom, dateTo: query.dateTo, salonID, page: query.page, limit: query.limit }))
+		console.log(data?.salonHistory)
 		dispatch(
 			initialize(FORM.SALON_HISTORY_FILTER, {
 				dateFromTo: {
@@ -57,64 +73,24 @@ const SalonHistory: FC<SalonSubPageProps> = (props) => {
 		fetchData()
 	}, [dispatch, query.page, query.limit, query.dateFrom, query.dateTo, query.interval])
 
-	const setIcon = (operation: SALON_HISTORY_OPERATIONS): undefined | ReactNode => {
-		switch (operation) {
-			case SALON_HISTORY_OPERATIONS.DELETE:
-				return <CloseIcon className={'history-icon danger'} />
-			case SALON_HISTORY_OPERATIONS.INSERT:
-				return <CheckIcon width={24} height={24} className={'history-icon success'} />
-			case SALON_HISTORY_OPERATIONS.RESTORE:
-				return <ResetIcon className={'history-icon info'} />
-			case SALON_HISTORY_OPERATIONS.UPDATE:
-				return <EditIcon className={'history-icon warning'} />
-			default:
-				return undefined
+	const renderValues = (oldValues: any, newValues: any) => {
+		let values: any = {}
+		if (oldValues) {
+			values = oldValues
+		} else if (newValues) {
+			values = newValues
 		}
-	}
 
-	const renderValues = (values: any, operation: SALON_HISTORY_OPERATIONS, showKey = false) => {
-		if (values && (operation === SALON_HISTORY_OPERATIONS.UPDATE || operation === SALON_HISTORY_OPERATIONS.INSERT)) {
-			return (
-				<Row gutter={[0, 0]}>
-					{' '}
-					{Object.keys(values).map((key: string) => {
-						if (typeof values[key] === 'object') {
-							if (showKey) {
-								return (
-									<>
-										<Col span={12}>{key}</Col>
-										<Col span={12}>
-											<p>{JSON.stringify(values[key])}</p>
-										</Col>
-									</>
-								)
-							}
-							return (
-								<Col span={24}>
-									<p>{JSON.stringify(values[key])}</p>
-								</Col>
-							)
-						}
-						if (showKey) {
-							return (
-								<>
-									<Col span={12}>{key}</Col>
-									<Col span={12}>
-										<p>{isEmpty(values[key]) && values[key] === '' && values[key] === null ? ' --- ' : `${values[key]}`}</p>
-									</Col>
-								</>
-							)
-						}
-						return (
-							<Col span={24}>
-								<p>{isEmpty(values[key]) || values[key] === '' || !values[key] ? ' --- ' : `${values[key]}`}</p>
-							</Col>
-						)
-					})}
-				</Row>
-			)
-		}
-		return undefined
+		return (
+			<>
+				{Object.keys(values)?.map((key: string) => {
+					if (typeof values?.[key] === 'object') {
+						return <CompareComponent valueKey={key} oldValue={JSON.stringify(oldValues?.[key])} newValue={JSON.stringify(newValues?.[key])} />
+					}
+					return <CompareComponent valueKey={key} oldValue={oldValues?.[key]} newValue={newValues?.[key]} />
+				})}
+			</>
+		)
 	}
 
 	const onChangePagination = (page: number, limit: number) => {
@@ -136,13 +112,8 @@ const SalonHistory: FC<SalonSubPageProps> = (props) => {
 		setQuery(newQuery)
 	}
 
-	return (
-		<>
-			<div className={'content-header block'}>
-				<SalonHistoryFilter onSubmit={handleSubmit} />
-			</div>
-			<Spin spinning={salonHistory.isLoading}>
-				<Timeline mode={'left'}>
+	/*
+	<Timeline mode={'left'}>
 					{salonHistory.data?.salonHistory.map((history) => (
 						<Timeline.Item
 							dot={setIcon(history.operation as SALON_HISTORY_OPERATIONS)}
@@ -170,6 +141,46 @@ const SalonHistory: FC<SalonSubPageProps> = (props) => {
 						</Timeline.Item>
 					))}
 				</Timeline>
+	*/
+
+	return (
+		<>
+			<div className={'content-header block'}>
+				<SalonHistoryFilter onSubmit={handleSubmit} />
+			</div>
+			<Spin spinning={salonHistory.isLoading}>
+				{salonHistory.data?.salonHistory.map((history) => (
+					<>
+						<List.Item key={history.id}>
+							<div className={'w-full'}>
+								<Divider className={'mb-1 mt-1'}>
+									<div className={'flex items-center justify-center'}>
+										<div>
+											<h4 className={'mr-2 mb-0'}>{formatDateByLocale(history.createdAt)}</h4>
+										</div>
+										{setIcon(history.operation as SALON_HISTORY_OPERATIONS)}
+										<div className={'flex items-center'}>
+											<h4
+												className={cx('m-0 p-0 history-text-action', {
+													warning: history.operation === SALON_HISTORY_OPERATIONS.UPDATE,
+													danger: history.operation === SALON_HISTORY_OPERATIONS.DELETE,
+													info: history.operation === SALON_HISTORY_OPERATIONS.RESTORE,
+													success: history.operation === SALON_HISTORY_OPERATIONS.INSERT
+												})}
+											>
+												{history.operation}
+											</h4>{' '}
+											<div className={'ml-2 font-bold'}>{history.userEmail}</div>
+										</div>
+									</div>
+								</Divider>
+								<div className={'flex items-center'}>
+									<div className={'w-full'}>{renderValues(history?.oldValue, history?.newValue)}</div>
+								</div>
+							</div>
+						</List.Item>
+					</>
+				))}
 			</Spin>
 			<div className={'content-footer pt-0 items-start'}>
 				<Row className={'w-full'} justify={'end'}>

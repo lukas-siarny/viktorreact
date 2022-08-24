8f78fd38a39fb813c8fc324acd0bc673cad130c8
Daniel Hlavaty <danielhlavaty@Daniels-MacBook-Pro.local>
2022-08-24 09:05:40 +0200

    NOT-1653 + config_overrides symlink

--- scripts/postBuild.js
+++ scripts/postBuild.js
@@ -15,12 +15,18 @@ try {
 	console.log('no gulpfile.js to delete')
 }
 
+try {
+	fs.unlinkSync(__dirname + "/../error_503/config-overrides.js")
+} catch {
+	console.log('no config-overrides to delete')
+}
+
 fs.symlink(__dirname + "/../tailwind.config.js",
         __dirname + '/../error_503/tailwind.config.js', 'file', (err) => {
   if (err)
     console.log(err);
   else {
-    console.log("\nSymlink created\n");
+    console.log("\nSymlink to tailwind.config.js created");
   }
 })
 
@@ -29,6 +35,15 @@ fs.symlink(__dirname + "/../gulpfile.js",
   if (err)
     console.log(err);
   else {
-    console.log("\nSymlink created\n");
+    console.log("\nSymlink to gulpfile.js created");
   }
+})
+
+fs.symlink(__dirname + "/../config-overrides.js",
+		__dirname + '/../error_503/config-overrides.js', 'file', (err) => {
+	if (err)
+		console.log(err);
+	else {
+		console.log('\nSymlink to config-overrides.js created')
+	}
 })

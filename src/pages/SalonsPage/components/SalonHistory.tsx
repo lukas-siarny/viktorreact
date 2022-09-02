8109aaca4c17ef37c0b8ee0076864b599049dc13
lsiarny <lukas.siarny@goodrequest.com>
2022-09-02 18:06:29 +0200

    NOT-2069: remove unused variables

--- src/pages/SalonsPage/components/SalonHistory.tsx
+++ src/pages/SalonsPage/components/SalonHistory.tsx
@@ -1,6 +1,6 @@
 import React, { FC, PropsWithChildren, ReactNode, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Divider, List, Row, Spin } from 'antd'
+import { Divider, List, Spin } from 'antd'
 import { NumberParam, StringParam, useQueryParams, withDefault } from 'use-query-params'
 import { initialize } from 'redux-form'
 import dayjs from 'dayjs'

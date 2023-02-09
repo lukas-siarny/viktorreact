5ffed6fa13011af3355de93a111ed0351a0b89ed
cvencek_jan <jan.cvencek@goodrequest.com>
2023-02-09 09:47:21 +0100

    Add Google authorization flow

--- src/pages/ReservationsSettingsPage/components/CalendarIntegrations.tsx
+++ src/pages/ReservationsSettingsPage/components/CalendarIntegrations.tsx
@@ -1,21 +1,52 @@
 import React from 'react'
-import { GoogleLogin } from '@react-oauth/google'
+import { GoogleLogin, useGoogleLogin } from '@react-oauth/google'
 import { useTranslation } from 'react-i18next'
+
+// utils
+import { postReq } from '../../../utils/request'
+import { NOTIFICATION_TYPE } from '../../../utils/enums'
 
 type Props = {}
 
 const CalendarIntegrations = (props: Props) => {
 	const { i18n } = useTranslation()
+
+	const login = useGoogleLogin({
+		// flow: 'auth-code',
+		onSuccess: (tokenResponse) => {
+			console.log({ tokenResponse })
+			postReq(
+				'/api/b2b/admin/calendar-sync/sync-token',
+				null,
+				{
+					refreshToken: tokenResponse.access_token, // tokenResponse.code,
+					calendarType: 'GOOGLE'
+				},
+				undefined,
+				NOTIFICATION_TYPE.NOTIFICATION,
+				true
+			)
+		},
+		onError: (errorResponse) => console.log('Error GAPI: ', errorResponse)
+	})
+
 	return (
-		<GoogleLogin
-			onSuccess={(credentialResponse) => {
-				console.log(credentialResponse)
-			}}
-			onError={() => {
-				console.log('Login Failed')
-			}}
-			locale={i18n.language}
-		/>
+		<>
+			<GoogleLogin
+				onSuccess={(credentialResponse) => {
+					console.log(credentialResponse)
+				}}
+				onError={() => {
+					console.log('Login Failed')
+				}}
+				locale={i18n.language}
+				type='icon'
+				theme='filled_black'
+			/>
+			<button onClick={() => login()} type='button'>
+				Log in to Google
+			</button>
+		</>
 	)
 }
 

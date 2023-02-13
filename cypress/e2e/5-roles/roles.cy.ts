4610534fe377a58bd531e5c949f2bacb73e79411
HaluskaRoman <roman.haluska@goodrequest.com>
2023-02-13 12:54:41 +0100

    NOT-4182 adding tests for roles

---
+++ cypress/e2e/5-roles/roles.cy.ts
@@ -0,0 +1,30 @@
+import { data } from '../../fixtures/roles.json'
+
+context('dynamic test for roles', () => {
+	data.forEach((user: any) => {
+		if (user.test.includes('1')) {
+			it('test 1', () => {
+				cy.log('user: ', user.credentials.user)
+				cy.log('password: ', user.credentials.password)
+			})
+		}
+		if (user.test.includes('2')) {
+			it('test 2', () => {
+				cy.log('user: ', user.credentials.user)
+				cy.log('password: ', user.credentials.password)
+			})
+		}
+		if (user.test.includes('3')) {
+			it('test 3', () => {
+				cy.log('user: ', user.credentials.user)
+				cy.log('password: ', user.credentials.password)
+			})
+		}
+		if (user.test.includes('4')) {
+			it('test 4', () => {
+				cy.log('user: ', user.credentials.user)
+				cy.log('password: ', user.credentials.password)
+			})
+		}
+	})
+})

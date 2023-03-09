ff59eb05ade52c861ac17f33545160b20ab9f501
lsiarny <lukas.siarny@goodrequest.com>
2023-03-09 11:26:44 +0100

    NOT-4559: fix translations

--- src/pages/Calendar/calendarHelpers.tsx
+++ src/pages/Calendar/calendarHelpers.tsx
@@ -369,7 +369,7 @@ const createEmployeeResourceData = (employee: CalendarEvent['employee'], isTimeO
 			id: employee.id,
 			firstName: employee.firstName,
 			lastName: employee?.lastName,
-			email: employee.email
+			email: employee.email || employee.inviteEmail
 		}),
 		color: employee.color,
 		image: employee.image.resizedImages.thumbnail,

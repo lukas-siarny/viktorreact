64ab349d2a3b30d4e31081b4848fda2829d01b2b
lsiarny <lukas.siarny@goodrequest.com>
2023-03-20 23:45:33 +0100

    NOT-4709: fix typo and add missing translations

--- src/pages/ActivationPage/components/ActivationForm.tsx
+++ src/pages/ActivationPage/components/ActivationForm.tsx
@@ -6,7 +6,7 @@ import { useTranslation } from 'react-i18next'
 // atoms
 import PinField from '../../../atoms/PinField'
 
-// shcemas
+// schemas
 import { IActivationForm, validationFn as validateActivationForm } from '../../../schemas/activation'
 
 // utils

677711323c660fe336e078680a6a81646551ad2c
lsiarny <lukas.siarny@goodrequest.com>
2023-04-20 10:40:23 +0200

    NOT-5081: remove default value of status_all query param in salons page + merge dev + typecheck for links from Notino dashboard to Salons page

--- src/pages/HomePage/components/NotinoDashboard.tsx
+++ src/pages/HomePage/components/NotinoDashboard.tsx
@@ -43,8 +43,11 @@ import {
 	SALONS_TIME_STATS_TYPE,
 	STRINGS
 } from '../../../utils/enums'
-import { formatObjToQuery } from '../../../utils/helper'
 import { doughnutOptions, lineOptions, getFilterRanges, transformToStatsData } from './dashboardUtils'
+
+// schema
+import { formatObjToQuery } from '../../../hooks/useQueryParamsZod'
+import { ISalonsPageURLQueryParams } from '../../../schemas/queryParams'
 
 ChartJS.register(ArcElement, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, annotationPlugin, BarElement)
 
@@ -250,7 +253,7 @@ const NotinoDashboard: FC = () => {
 	const [tabKey, setTabKey] = useState<DASHBOARD_TAB_KEYS>(DASHBOARD_TAB_KEYS.SALONS_STATE)
 
 	const publishedPremiumSalonsData = useMemo(() => {
-		const query = {
+		const query: ISalonsPageURLQueryParams = {
 			salonState: SALONS_TAB_KEYS.ACTIVE,
 			statuses_published: SALON_FILTER_STATES.PUBLISHED,
 			createType: SALON_CREATE_TYPE.NON_BASIC
@@ -279,7 +282,7 @@ const NotinoDashboard: FC = () => {
 					onClick: () =>
 						navigate({
 							pathname: t('paths:salons'),
-							search: formatObjToQuery({ ...query, enabledReservationsSetting: SALON_FILTER_RS.NOT_ENABLED })
+							search: formatObjToQuery<ISalonsPageURLQueryParams>({ ...query, enabledReservationsSetting: SALON_FILTER_RS.NOT_ENABLED })
 						}),
 					backgroundColor: '#144896'
 				},
@@ -289,7 +292,7 @@ const NotinoDashboard: FC = () => {
 					onClick: () =>
 						navigate({
 							pathname: t('paths:salons'),
-							search: formatObjToQuery({ ...query, enabledReservationsSetting: SALON_FILTER_RS.ENABLED })
+							search: formatObjToQuery<ISalonsPageURLQueryParams>({ ...query, enabledReservationsSetting: SALON_FILTER_RS.ENABLED })
 						}),
 					backgroundColor: '#2277F3'
 				},
@@ -299,7 +302,7 @@ const NotinoDashboard: FC = () => {
 					onClick: () =>
 						navigate({
 							pathname: t('paths:salons'),
-							search: formatObjToQuery({
+							search: formatObjToQuery<ISalonsPageURLQueryParams>({
 								...query,
 								enabledReservationsSetting: SALON_FILTER_RS.ENABLED,
 								hasAvailableReservationSystem: SALON_FILTER_RS_AVAILABLE_ONLINE.AVAILABLE
@@ -319,7 +322,7 @@ const NotinoDashboard: FC = () => {
 	])
 
 	const unpublishedPremiumSalonsData = useMemo(() => {
-		const query = {
+		const query: ISalonsPageURLQueryParams = {
 			salonState: SALONS_TAB_KEYS.ACTIVE,
 			statuses_published: SALON_FILTER_STATES.NOT_PUBLISHED,
 			createType: SALON_CREATE_TYPE.NON_BASIC
@@ -348,7 +351,7 @@ const NotinoDashboard: FC = () => {
 					onClick: () =>
 						navigate({
 							pathname: t('paths:salons'),
-							search: formatObjToQuery({ ...query, enabledReservationsSetting: SALON_FILTER_RS.NOT_ENABLED })
+							search: formatObjToQuery<ISalonsPageURLQueryParams>({ ...query, enabledReservationsSetting: SALON_FILTER_RS.NOT_ENABLED })
 						}),
 					backgroundColor: '#144896'
 				},
@@ -358,7 +361,7 @@ const NotinoDashboard: FC = () => {
 					onClick: () =>
 						navigate({
 							pathname: t('paths:salons'),
-							search: formatObjToQuery({ ...query, enabledReservationsSetting: SALON_FILTER_RS.ENABLED })
+							search: formatObjToQuery<ISalonsPageURLQueryParams>({ ...query, enabledReservationsSetting: SALON_FILTER_RS.ENABLED })
 						}),
 					backgroundColor: '#2277F3'
 				},
@@ -368,7 +371,7 @@ const NotinoDashboard: FC = () => {
 					onClick: () =>
 						navigate({
 							pathname: t('paths:salons'),
-							search: formatObjToQuery({
+							search: formatObjToQuery<ISalonsPageURLQueryParams>({
 								...query,
 								enabledReservationsSetting: SALON_FILTER_RS.ENABLED,
 								hasAvailableReservationSystem: SALON_FILTER_RS_AVAILABLE_ONLINE.AVAILABLE

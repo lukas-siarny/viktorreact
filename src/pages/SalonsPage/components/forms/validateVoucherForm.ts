177f26b34799fe190a2e6c01e03214945ba6712f
Michal Bella <michal.bella@goodrequest.com>
2023-05-03 11:55:48 +0200

    removed validation for length

--- src/pages/SalonsPage/components/forms/validateVoucherForm.ts
+++ src/pages/SalonsPage/components/forms/validateVoucherForm.ts
@@ -1,6 +1,6 @@
 import { FormErrors } from 'redux-form'
 import i18next from 'i18next'
-import { VALIDATION_MAX_LENGTH } from '../../../../utils/enums'
+
 import { IVoucherForm } from '../../../../types/interfaces'
 
 export default (values: IVoucherForm) => {
@@ -8,13 +8,6 @@ export default (values: IVoucherForm) => {
 
 	if (!values?.code) {
 		errors.code = i18next.t('loc:Toto pole je povinné')
-	}
-	// TODO: syncnut s BE
-
-	if (values?.code && values.code?.length > VALIDATION_MAX_LENGTH.LENGTH_1000) {
-		errors.code = i18next.t('loc:Max. počet znakov je {{max}}', {
-			max: VALIDATION_MAX_LENGTH.LENGTH_1000
-		})
 	}
 
 	return errors

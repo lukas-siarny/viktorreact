a4581764e8d2dcf71a322443f5164ffb44666023
lsiarny <lukas.siarny@goodrequest.com>
2023-05-16 19:08:30 +0200

    NOT-5400: logout user before registration

--- src/routes/PublicRoute.tsx
+++ src/routes/PublicRoute.tsx
@@ -1,9 +1,12 @@
-import React, { FC } from 'react'
-import { RouteProps, Navigate } from 'react-router-dom'
+import React, { FC, useEffect } from 'react'
+import { RouteProps, Navigate, useLocation } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
+import queryString from 'query-string'
+import { useDispatch } from 'react-redux'
 
 import BaseRoute from './BaseRoute'
 import { isLoggedIn } from '../utils/auth'
+import { logOutUser } from '../reducers/users/userActions'
 
 type Props = RouteProps & {
 	layout?: any
@@ -17,8 +20,20 @@ type Props = RouteProps & {
 
 const PublicRoute: FC<Props> = (props) => {
 	const [t] = useTranslation()
+	const dispatch = useDispatch()
+	const { search } = useLocation()
+
 	const { redirectLoggedInUser = true } = props
-	if (isLoggedIn() && redirectLoggedInUser) {
+
+	const { logout } = queryString.parse(search, { decode: false })
+
+	useEffect(() => {
+		if (logout === 'true' && isLoggedIn()) {
+			dispatch(logOutUser(true))
+		}
+	}, [logout, dispatch])
+
+	if (isLoggedIn() && redirectLoggedInUser && logout !== 'true') {
 		return <Navigate to={t('paths:index')} />
 	}
 

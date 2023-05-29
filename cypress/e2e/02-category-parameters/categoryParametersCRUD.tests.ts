3be4b036bb6dfc01a17d8ebde1caf14d96eea94c
lsiarny <lukas.siarny@goodrequest.com>
2023-05-29 23:33:09 +0200

    NOT-5089: update cosmetics, languages and category parameters tests

--- cypress/e2e/02-category-parameters/categoryParametersCRUD.tests.ts
+++ cypress/e2e/02-category-parameters/categoryParametersCRUD.tests.ts
@@ -29,6 +29,10 @@ const categoryParameterCRUDTestSuite = (actions: CRUD_OPERATIONS[], email?: stri
 			method: 'POST',
 			url: '/api/b2b/admin/enums/category-parameters/'
 		}).as('createCategoryParameters')
+		cy.intercept({
+			method: 'GET',
+			url: '/api/b2b/admin/enums/category-parameters/'
+		}).as('getCategoryParams')
 		cy.visit('/category-parameters/create')
 		if (actions.includes(CRUD_OPERATIONS.ALL) || actions.includes(CRUD_OPERATIONS.CREATE)) {
 			cy.setInputValue(FORM.CATEGORY_PARAMS, 'nameLocalizations-0-value', category.parameter.create.title)
@@ -67,8 +71,8 @@ const categoryParameterCRUDTestSuite = (actions: CRUD_OPERATIONS[], email?: stri
 			method: 'POST',
 			url: `/api/b2b/admin/enums/category-parameters/${categoryParameterID}/values/`
 		}).as('createCategoryParameterValue')
-		cy.visit(`/category-parameters/${categoryParameterID}`)
-		if (actions.includes(CRUD_OPERATIONS.ALL) || actions.includes(CRUD_OPERATIONS.CREATE) || actions.includes(CRUD_OPERATIONS.READ)) {
+		if (actions.includes(CRUD_OPERATIONS.ALL) || actions.includes(CRUD_OPERATIONS.CREATE)) {
+			cy.visit(`/category-parameters/${categoryParameterID}`)
 			cy.wait('@getCategoryParameters').then((interceptorGetCategoryParameters: any) => {
 				// check status code
 				expect(interceptorGetCategoryParameters.response.statusCode).to.equal(200)
@@ -77,18 +81,46 @@ const categoryParameterCRUDTestSuite = (actions: CRUD_OPERATIONS[], email?: stri
 				cy.clickButton(ADD_BUTTON_ID, FORM.CATEGORY_PARAMS)
 				cy.setInputValue(FORM.CATEGORY_PARAMS, 'localizedValues-2-valueLocalizations-0-value', category.value.create['value-3'], false, true)
 				cy.clickButton(SUBMIT_BUTTON_ID, FORM.CATEGORY_PARAMS)
-				if (actions.includes(CRUD_OPERATIONS.ALL) || actions.includes(CRUD_OPERATIONS.CREATE)) {
-					cy.wait('@createCategoryParameterValue').then((interception: any) => {
-						// check status code of login request
-						expect(interception.response.statusCode).to.equal(200)
-						// check conf toast message
-						cy.checkSuccessToastMessage()
-					})
-				} else {
-					cy.checkForbiddenModal()
-				}
+				cy.wait('@createCategoryParameterValue').then((interception: any) => {
+					// check status code of login request
+					expect(interception.response.statusCode).to.equal(200)
+					// check conf toast message
+					cy.checkSuccessToastMessage()
+				})
+			})
+		} else if (actions.includes(CRUD_OPERATIONS.READ)) {
+			cy.visit('/category-parameters')
+			cy.wait('@getCategoryParams').then((interception: any) => {
+				// check status code
+				expect(interception.response.statusCode).to.equal(200)
+
+				// open first category param from table
+				cy.get('main.ant-layout-content').then(($body) => {
+					if ($body.find('.ant-table .ant-table-row:first').length) {
+						cy.get('.ant-table .ant-table-row:first')
+							.as('detailRow')
+							.invoke('attr', 'data-row-key')
+							.then((dataRowKey) => {
+								const paramsID = dataRowKey || ''
+								cy.intercept({
+									method: 'GET',
+									url: `/api/b2b/admin/enums/category-parameters/${paramsID}`
+								}).as('getCategoryParametersForReadPermission')
+								cy.wait('@getCategoryParametersForReadPermission').then((intreceptionDetail: any) => {
+									expect(intreceptionDetail.response.statusCode).to.equal(200)
+									cy.clickButton(ADD_BUTTON_ID, FORM.CATEGORY_PARAMS)
+									cy.setInputValue(FORM.CATEGORY_PARAMS, 'localizedValues-1-valueLocalizations-0-value', category.value.create['value-2'], false, true)
+									cy.clickButton(ADD_BUTTON_ID, FORM.CATEGORY_PARAMS)
+									cy.setInputValue(FORM.CATEGORY_PARAMS, 'localizedValues-2-valueLocalizations-0-value', category.value.create['value-3'], false, true)
+									cy.clickButton(SUBMIT_BUTTON_ID, FORM.CATEGORY_PARAMS)
+									cy.checkForbiddenModal()
+								})
+							})
+					}
+				})
 			})
 		} else {
+			cy.visit(`/category-parameters/${categoryParameterID}`)
 			// check redirect to 403 unauthorized page
 			cy.location('pathname').should('eq', '/403')
 		}
@@ -106,7 +138,7 @@ const categoryParameterCRUDTestSuite = (actions: CRUD_OPERATIONS[], email?: stri
 		}).as('updateCategoryParameterValue')
 		cy.visit(`/category-parameters/${categoryParameterID}`)
 
-		if (actions.includes(CRUD_OPERATIONS.ALL) || actions.includes(CRUD_OPERATIONS.UPDATE) || actions.includes(CRUD_OPERATIONS.READ)) {
+		if (actions.includes(CRUD_OPERATIONS.ALL) || actions.includes(CRUD_OPERATIONS.UPDATE)) {
 			cy.wait('@getCategoryParameters').then((interceptorGetCategoryParameters: any) => {
 				// check status code
 				expect(interceptorGetCategoryParameters.response.statusCode).to.equal(200)
@@ -114,18 +146,45 @@ const categoryParameterCRUDTestSuite = (actions: CRUD_OPERATIONS[], email?: stri
 				cy.setInputValue(FORM.CATEGORY_PARAMS, 'localizedValues-1-valueLocalizations-0-value', category.value.update['value-2'], false, true)
 				cy.setInputValue(FORM.CATEGORY_PARAMS, 'localizedValues-2-valueLocalizations-0-value', category.value.update['value-3'], false, true)
 				cy.clickButton(SUBMIT_BUTTON_ID, FORM.CATEGORY_PARAMS)
-				if (actions.includes(CRUD_OPERATIONS.ALL) || actions.includes(CRUD_OPERATIONS.UPDATE)) {
-					cy.wait('@updateCategoryParameterValue').then((interception: any) => {
-						// check status code of login request
-						expect(interception.response.statusCode).to.equal(200)
-						// check conf toast message
-						cy.checkSuccessToastMessage()
-					})
-				} else {
-					cy.checkForbiddenModal()
-				}
+				cy.wait('@updateCategoryParameterValue').then((interception: any) => {
+					// check status code of login request
+					expect(interception.response.statusCode).to.equal(200)
+					// check conf toast message
+					cy.checkSuccessToastMessage()
+				})
+			})
+		} else if (actions.includes(CRUD_OPERATIONS.READ)) {
+			cy.visit('/category-parameters')
+			cy.wait('@getCategoryParams').then((interception: any) => {
+				// check status code
+				expect(interception.response.statusCode).to.equal(200)
+
+				// open first category param from table
+				cy.get('main.ant-layout-content').then(($body) => {
+					if ($body.find('.ant-table .ant-table-row:first').length) {
+						cy.get('.ant-table .ant-table-row:first')
+							.as('detailRow')
+							.invoke('attr', 'data-row-key')
+							.then((dataRowKey) => {
+								const paramsID = dataRowKey || ''
+								cy.intercept({
+									method: 'GET',
+									url: `/api/b2b/admin/enums/category-parameters/${paramsID}`
+								}).as('getCategoryParametersForReadPermission')
+								cy.wait('@getCategoryParametersForReadPermission').then((intreceptionDetail: any) => {
+									expect(intreceptionDetail.response.statusCode).to.equal(200)
+									cy.setInputValue(FORM.CATEGORY_PARAMS, 'localizedValues-0-valueLocalizations-0-value', category.value.update['value-1'], false, true)
+									cy.setInputValue(FORM.CATEGORY_PARAMS, 'localizedValues-1-valueLocalizations-0-value', category.value.update['value-2'], false, true)
+									cy.setInputValue(FORM.CATEGORY_PARAMS, 'localizedValues-2-valueLocalizations-0-value', category.value.update['value-3'], false, true)
+									cy.clickButton(SUBMIT_BUTTON_ID, FORM.CATEGORY_PARAMS)
+									cy.checkForbiddenModal()
+								})
+							})
+					}
+				})
 			})
 		} else {
+			cy.visit(`/category-parameters/${categoryParameterID}`)
 			// check redirect to 403 unauthorized page
 			cy.location('pathname').should('eq', '/403')
 		}
@@ -141,25 +200,49 @@ const categoryParameterCRUDTestSuite = (actions: CRUD_OPERATIONS[], email?: stri
 			method: 'DELETE',
 			url: `/api/b2b/admin/enums/category-parameters/${categoryParameterID}/values/*`
 		}).as('deleteCategoryParameterValue')
-		cy.visit(`/category-parameters/${categoryParameterID}`)
-		if (actions.includes(CRUD_OPERATIONS.ALL) || actions.includes(CRUD_OPERATIONS.DELETE || actions.includes(CRUD_OPERATIONS.READ))) {
+		if (actions.includes(CRUD_OPERATIONS.ALL) || actions.includes(CRUD_OPERATIONS.DELETE)) {
+			cy.visit(`/category-parameters/${categoryParameterID}`)
 			cy.wait('@getCategoryParameters').then((interceptorGetCategoryParameters: any) => {
 				// check status code
 				expect(interceptorGetCategoryParameters.response.statusCode).to.equal(200)
 				cy.clickDeleteButtonWithConfCustom(FORM.CATEGORY_PARAMS, `${DELETE_BUTTON_ID}-${0}`)
 
-				if (actions.includes(CRUD_OPERATIONS.ALL) || actions.includes(CRUD_OPERATIONS.DELETE)) {
-					cy.wait('@deleteCategoryParameterValue').then((interception: any) => {
-						// check status code of login request
-						expect(interception.response.statusCode).to.equal(200)
-						// check conf toast message
-						cy.checkSuccessToastMessage()
-					})
-				} else {
-					cy.checkForbiddenModal()
-				}
+				cy.wait('@deleteCategoryParameterValue').then((interception: any) => {
+					// check status code of login request
+					expect(interception.response.statusCode).to.equal(200)
+					// check conf toast message
+					cy.checkSuccessToastMessage()
+				})
+			})
+		} else if (actions.includes(CRUD_OPERATIONS.READ)) {
+			cy.visit('/category-parameters')
+			cy.wait('@getCategoryParams').then((interception: any) => {
+				// check status code
+				expect(interception.response.statusCode).to.equal(200)
+
+				// open first category param from table
+				cy.get('main.ant-layout-content').then(($body) => {
+					if ($body.find('.ant-table .ant-table-row:first').length) {
+						cy.get('.ant-table .ant-table-row:first')
+							.as('detailRow')
+							.invoke('attr', 'data-row-key')
+							.then((dataRowKey) => {
+								const paramsID = dataRowKey || ''
+								cy.intercept({
+									method: 'GET',
+									url: `/api/b2b/admin/enums/category-parameters/${paramsID}`
+								}).as('getCategoryParametersForReadPermission')
+								cy.wait('@getCategoryParametersForReadPermission').then((intreceptionDetail: any) => {
+									expect(intreceptionDetail.response.statusCode).to.equal(200)
+									cy.get(`#${FORM.CATEGORY_PARAMS}-delete-btn`).click()
+									cy.checkForbiddenModal()
+								})
+							})
+					}
+				})
 			})
 		} else {
+			cy.visit(`/category-parameters/${categoryParameterID}`)
 			// check redirect to 403 unauthorized page
 			cy.location('pathname').should('eq', '/403')
 		}
@@ -175,26 +258,51 @@ const categoryParameterCRUDTestSuite = (actions: CRUD_OPERATIONS[], email?: stri
 			method: 'PATCH',
 			url: `/api/b2b/admin/enums/category-parameters/${categoryParameterID}`
 		}).as('updateCategoryParameters')
-		cy.visit(`/category-parameters/${categoryParameterID}`)
-		if (actions.includes(CRUD_OPERATIONS.ALL) || actions.includes(CRUD_OPERATIONS.UPDATE) || actions.includes(CRUD_OPERATIONS.READ)) {
+		if (actions.includes(CRUD_OPERATIONS.ALL) || actions.includes(CRUD_OPERATIONS.UPDATE)) {
+			cy.visit(`/category-parameters/${categoryParameterID}`)
 			cy.wait('@getCategoryParameters').then((interceptorGetCategoryParameters: any) => {
 				// check status code
 				expect(interceptorGetCategoryParameters.response.statusCode).to.equal(200)
 				cy.setInputValue(FORM.CATEGORY_PARAMS, 'nameLocalizations-0-value', category.parameter.update.title, false, true)
 				cy.clickButton(SUBMIT_BUTTON_ID, FORM.CATEGORY_PARAMS)
 
-				if (actions.includes(CRUD_OPERATIONS.ALL) || actions.includes(CRUD_OPERATIONS.UPDATE)) {
-					cy.wait('@updateCategoryParameters').then((interception: any) => {
-						// check status code of login request
-						expect(interception.response.statusCode).to.equal(200)
-						// check conf toast message
-						cy.checkSuccessToastMessage()
-					})
-				} else {
-					cy.checkForbiddenModal()
-				}
+				cy.wait('@updateCategoryParameters').then((interception: any) => {
+					// check status code of login request
+					expect(interception.response.statusCode).to.equal(200)
+					// check conf toast message
+					cy.checkSuccessToastMessage()
+				})
+			})
+		} else if (actions.includes(CRUD_OPERATIONS.READ)) {
+			cy.visit('/category-parameters')
+			cy.wait('@getCategoryParams').then((interception: any) => {
+				// check status code
+				expect(interception.response.statusCode).to.equal(200)
+
+				// open first category param from table
+				cy.get('main.ant-layout-content').then(($body) => {
+					if ($body.find('.ant-table .ant-table-row:first').length) {
+						cy.get('.ant-table .ant-table-row:first')
+							.as('detailRow')
+							.invoke('attr', 'data-row-key')
+							.then((dataRowKey) => {
+								const paramsID = dataRowKey || ''
+								cy.intercept({
+									method: 'GET',
+									url: `/api/b2b/admin/enums/category-parameters/${paramsID}`
+								}).as('getCategoryParametersForReadPermission')
+								cy.wait('@getCategoryParametersForReadPermission').then((intreceptionDetail: any) => {
+									expect(intreceptionDetail.response.statusCode).to.equal(200)
+									cy.setInputValue(FORM.CATEGORY_PARAMS, 'nameLocalizations-0-value', category.parameter.update.title, false, true)
+									cy.clickButton(SUBMIT_BUTTON_ID, FORM.CATEGORY_PARAMS)
+									cy.checkForbiddenModal()
+								})
+							})
+					}
+				})
 			})
 		} else {
+			cy.visit(`/category-parameters/${categoryParameterID}`)
 			// check redirect to 403 unauthorized page
 			cy.location('pathname').should('eq', '/403')
 		}
@@ -210,27 +318,51 @@ const categoryParameterCRUDTestSuite = (actions: CRUD_OPERATIONS[], email?: stri
 			method: 'DELETE',
 			url: `/api/b2b/admin/enums/category-parameters/${categoryParameterID}`
 		}).as('deleteCategoryParameters')
-		cy.visit(`/category-parameters/${categoryParameterID}`)
-		if (actions.includes(CRUD_OPERATIONS.ALL) || actions.includes(CRUD_OPERATIONS.DELETE) || actions.includes(CRUD_OPERATIONS.READ)) {
+		if (actions.includes(CRUD_OPERATIONS.ALL) || actions.includes(CRUD_OPERATIONS.DELETE)) {
+			cy.visit(`/category-parameters/${categoryParameterID}`)
 			cy.wait('@getCategoryParameters').then((interceptorGetCategoryParameters: any) => {
 				// check status code
 				expect(interceptorGetCategoryParameters.response.statusCode).to.equal(200)
 
 				cy.clickDeleteButtonWithConfCustom(FORM.CATEGORY_PARAMS)
 
-				if (actions.includes(CRUD_OPERATIONS.ALL) || actions.includes(CRUD_OPERATIONS.DELETE)) {
-					cy.wait('@deleteCategoryParameters').then((interception: any) => {
-						// check status code
-						expect(interception.response.statusCode).to.equal(200)
-						// check conf toast message
-						cy.checkSuccessToastMessage()
-						cy.location('pathname').should('eq', '/category-parameters')
-					})
-				} else {
-					cy.checkForbiddenModal()
-				}
+				cy.wait('@deleteCategoryParameters').then((interception: any) => {
+					// check status code
+					expect(interception.response.statusCode).to.equal(200)
+					// check conf toast message
+					cy.checkSuccessToastMessage()
+					cy.location('pathname').should('eq', '/category-parameters')
+				})
+			})
+		} else if (actions.includes(CRUD_OPERATIONS.READ)) {
+			cy.visit('/category-parameters')
+			cy.wait('@getCategoryParams').then((interception: any) => {
+				// check status code
+				expect(interception.response.statusCode).to.equal(200)
+
+				// open first category param from table
+				cy.get('main.ant-layout-content').then(($body) => {
+					if ($body.find('.ant-table .ant-table-row:first').length) {
+						cy.get('.ant-table .ant-table-row:first')
+							.as('detailRow')
+							.invoke('attr', 'data-row-key')
+							.then((dataRowKey) => {
+								const paramsID = dataRowKey || ''
+								cy.intercept({
+									method: 'GET',
+									url: `/api/b2b/admin/enums/category-parameters/${paramsID}`
+								}).as('getCategoryParametersForReadPermission')
+								cy.wait('@getCategoryParametersForReadPermission').then((intreceptionDetail: any) => {
+									expect(intreceptionDetail.response.statusCode).to.equal(200)
+									cy.get(`#${FORM.CATEGORY_PARAMS}-delete-btn`).click()
+									cy.checkForbiddenModal()
+								})
+							})
+					}
+				})
 			})
 		} else {
+			cy.visit(`/category-parameters/${categoryParameterID}`)
 			// check redirect to 403 unauthorized page
 			cy.location('pathname').should('eq', '/403')
 		}
